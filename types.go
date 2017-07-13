b0f20f8dcf04ac31cc2b9a6dba4a58440a0a5c56
Oleksiy Chechel <alex.mirrr@gmail.com>
2017-07-13 12:28:05 +0700

    Fix numbers conversion (cast to builtin types)

--- types.go
+++ types.go
@@ -20,56 +20,194 @@ type (
 )
 
 // String convert interface{} type to string if it posible
-func String(s interface{}) (r string) {
+func String(s interface{}) string {
 	if s == nil {
-		r = ""
-		return
+		return ""
 	}
 	switch v := s.(type) {
 	case string:
-		r = v
+		return v
+	case *string:
+		return *v
 	case []byte:
-		r = string(v)
+		return string(v)
+	case *[]byte:
+		return string(*v)
 	case []string:
-		r = strings.Join(v, "")
-	default:
-		r = fmt.Sprintf("%v", v)
+		return strings.Join(v, "")
+	case *[]string:
+		return strings.Join(*v, "")
 	}
-	return
+
+	return fmt.Sprintf("%v", s)
 }
 
 // Int converting
-func Int(s interface{}) (r int) {
-	tmpr, _ := strconv.ParseInt(String(s), 10, 64)
-	r = int(tmpr)
-	return
+func Int(s interface{}) int {
+	switch v := s.(type) {
+	case int:
+		return int(v)
+	case int8:
+		return int(v)
+	case int16:
+		return int(v)
+	case int32:
+		return int(v)
+	case int64:
+		return int(v)
+	case uint:
+		return int(v)
+	case uint8:
+		return int(v)
+	case uint16:
+		return int(v)
+	case uint32:
+		return int(v)
+	case uint64:
+		return int(v)
+	case float32:
+		return int(v)
+	case float64:
+		return int(v)
+	}
+
+	tmpr, _ := strconv.Atoi(String(s))
+	return tmpr
 }
 
 // Bool converting
-func Bool(s interface{}) (r bool) {
-	r, _ = strconv.ParseBool(String(s))
-	return
+func Bool(s interface{}) bool {
+	switch v := s.(type) {
+	case bool:
+		return v
+	case int:
+		return v != 0
+	case int8:
+		return v != 0
+	case int16:
+		return v != 0
+	case int32:
+		return v != 0
+	case int64:
+		return v != 0
+	case uint:
+		return v != 0
+	case uint8:
+		return v != 0
+	case uint16:
+		return v != 0
+	case uint32:
+		return v != 0
+	case uint64:
+		return v != 0
+	case float32:
+		return v != 0
+	case float64:
+		return v != 0
+	}
+
+	r, _ := strconv.ParseBool(String(s))
+	return r
 }
 
 // Int8 converting
-func Int8(s interface{}) (r int8) {
+func Int8(s interface{}) int8 {
+	switch v := s.(type) {
+	case int:
+		return int8(v)
+	case int8:
+		return int8(v)
+	case int16:
+		return int8(v)
+	case int32:
+		return int8(v)
+	case int64:
+		return int8(v)
+	case uint:
+		return int8(v)
+	case uint8:
+		return int8(v)
+	case uint16:
+		return int8(v)
+	case uint32:
+		return int8(v)
+	case uint64:
+		return int8(v)
+	case float32:
+		return int8(v)
+	case float64:
+		return int8(v)
+	}
+
 	tmpr, _ := strconv.ParseInt(String(s), 10, 8)
-	r = int8(tmpr)
-	return
+	return int8(tmpr)
 }
 
 // Int16 converting
-func Int16(s interface{}) (r int16) {
+func Int16(s interface{}) int16 {
+	switch v := s.(type) {
+	case int:
+		return int16(v)
+	case int8:
+		return int16(v)
+	case int16:
+		return int16(v)
+	case int32:
+		return int16(v)
+	case int64:
+		return int16(v)
+	case uint:
+		return int16(v)
+	case uint8:
+		return int16(v)
+	case uint16:
+		return int16(v)
+	case uint32:
+		return int16(v)
+	case uint64:
+		return int16(v)
+	case float32:
+		return int16(v)
+	case float64:
+		return int16(v)
+	}
+
 	tmpr, _ := strconv.ParseInt(String(s), 10, 16)
-	r = int16(tmpr)
-	return
+	return int16(tmpr)
+
 }
 
 // Int32 converting
-func Int32(s interface{}) (r int32) {
+func Int32(s interface{}) int32 {
+	switch v := s.(type) {
+	case int:
+		return int32(v)
+	case int8:
+		return int32(v)
+	case int16:
+		return int32(v)
+	case int32:
+		return int32(v)
+	case int64:
+		return int32(v)
+	case uint:
+		return int32(v)
+	case uint8:
+		return int32(v)
+	case uint16:
+		return int32(v)
+	case uint32:
+		return int32(v)
+	case uint64:
+		return int32(v)
+	case float32:
+		return int32(v)
+	case float64:
+		return int32(v)
+	}
+
 	tmpr, _ := strconv.ParseInt(String(s), 10, 32)
-	r = int32(tmpr)
-	return
+	return int32(tmpr)
 }
 
 // Rune converting (int32)
@@ -79,62 +217,272 @@ func Rune(s interface{}) (r rune) {
 }
 
 // Int64 converting
-func Int64(s interface{}) (r int64) {
-	r, _ = strconv.ParseInt(String(s), 10, 64)
-	return
+func Int64(s interface{}) int64 {
+	switch v := s.(type) {
+	case int:
+		return int64(v)
+	case int8:
+		return int64(v)
+	case int16:
+		return int64(v)
+	case int32:
+		return int64(v)
+	case int64:
+		return int64(v)
+	case uint:
+		return int64(v)
+	case uint8:
+		return int64(v)
+	case uint16:
+		return int64(v)
+	case uint32:
+		return int64(v)
+	case uint64:
+		return int64(v)
+	case float32:
+		return int64(v)
+	case float64:
+		return int64(v)
+	}
+
+	r, _ := strconv.ParseInt(String(s), 10, 64)
+	return r
 }
 
 // Byte converting
-func Byte(s interface{}) (r byte) {
-	tmpr, _ := strconv.ParseUint(String(s), 10, 8)
-	r = byte(tmpr)
-	return
+func Byte(s interface{}) byte {
+	return byte(Uint8(s))
 }
 
 // Uint converting
-func Uint(s interface{}) (r uint) {
+func Uint(s interface{}) uint {
+	switch v := s.(type) {
+	case int:
+		return uint(v)
+	case int8:
+		return uint(v)
+	case int16:
+		return uint(v)
+	case int32:
+		return uint(v)
+	case int64:
+		return uint(v)
+	case uint:
+		return uint(v)
+	case uint8:
+		return uint(v)
+	case uint16:
+		return uint(v)
+	case uint32:
+		return uint(v)
+	case uint64:
+		return uint(v)
+	case float32:
+		return uint(v)
+	case float64:
+		return uint(v)
+	}
+
 	tmpr, _ := strconv.ParseUint(String(s), 10, 64)
-	r = uint(tmpr)
-	return
+	return uint(tmpr)
 }
 
 // Uint8 converting
-func Uint8(s interface{}) (r uint8) {
-	r = Byte(s)
-	return
+func Uint8(s interface{}) uint8 {
+	switch v := s.(type) {
+	case int:
+		return uint8(v)
+	case int8:
+		return uint8(v)
+	case int16:
+		return uint8(v)
+	case int32:
+		return uint8(v)
+	case int64:
+		return uint8(v)
+	case uint:
+		return uint8(v)
+	case uint8:
+		return uint8(v)
+	case uint16:
+		return uint8(v)
+	case uint32:
+		return uint8(v)
+	case uint64:
+		return uint8(v)
+	case float32:
+		return uint8(v)
+	case float64:
+		return uint8(v)
+	}
+
+	tmpr, _ := strconv.ParseUint(String(s), 10, 8)
+	return uint8(tmpr)
 }
 
 // Uint16 converting
-func Uint16(s interface{}) (r uint16) {
+func Uint16(s interface{}) uint16 {
+	switch v := s.(type) {
+	case int:
+		return uint16(v)
+	case int8:
+		return uint16(v)
+	case int16:
+		return uint16(v)
+	case int32:
+		return uint16(v)
+	case int64:
+		return uint16(v)
+	case uint:
+		return uint16(v)
+	case uint8:
+		return uint16(v)
+	case uint16:
+		return uint16(v)
+	case uint32:
+		return uint16(v)
+	case uint64:
+		return uint16(v)
+	case float32:
+		return uint16(v)
+	case float64:
+		return uint16(v)
+	}
+
 	tmpr, _ := strconv.ParseUint(String(s), 10, 16)
-	r = uint16(tmpr)
-	return
+	return uint16(tmpr)
 }
 
 // Uint32 converting
-func Uint32(s interface{}) (r uint32) {
+func Uint32(s interface{}) uint32 {
+	switch v := s.(type) {
+	case int:
+		return uint32(v)
+	case int8:
+		return uint32(v)
+	case int16:
+		return uint32(v)
+	case int32:
+		return uint32(v)
+	case int64:
+		return uint32(v)
+	case uint:
+		return uint32(v)
+	case uint8:
+		return uint32(v)
+	case uint16:
+		return uint32(v)
+	case uint32:
+		return uint32(v)
+	case uint64:
+		return uint32(v)
+	case float32:
+		return uint32(v)
+	case float64:
+		return uint32(v)
+	}
+
 	tmpr, _ := strconv.ParseUint(String(s), 10, 32)
-	r = uint32(tmpr)
-	return
+	return uint32(tmpr)
 }
 
 // Uint64 converting
 func Uint64(s interface{}) (r uint64) {
+	switch v := s.(type) {
+	case int:
+		return uint64(v)
+	case int8:
+		return uint64(v)
+	case int16:
+		return uint64(v)
+	case int32:
+		return uint64(v)
+	case int64:
+		return uint64(v)
+	case uint:
+		return uint64(v)
+	case uint8:
+		return uint64(v)
+	case uint16:
+		return uint64(v)
+	case uint32:
+		return uint64(v)
+	case uint64:
+		return uint64(v)
+	case float32:
+		return uint64(v)
+	case float64:
+		return uint64(v)
+	}
+
 	r, _ = strconv.ParseUint(String(s), 10, 64)
 	return
 }
 
 // Float32 converting
-func Float32(s interface{}) (r float32) {
+func Float32(s interface{}) float32 {
+	switch v := s.(type) {
+	case int:
+		return float32(v)
+	case int8:
+		return float32(v)
+	case int16:
+		return float32(v)
+	case int32:
+		return float32(v)
+	case int64:
+		return float32(v)
+	case uint:
+		return float32(v)
+	case uint8:
+		return float32(v)
+	case uint16:
+		return float32(v)
+	case uint32:
+		return float32(v)
+	case uint64:
+		return float32(v)
+	case float32:
+		return float32(v)
+	case float64:
+		return float32(v)
+	}
+
 	tmpr, _ := strconv.ParseFloat(String(s), 32)
-	r = float32(tmpr)
-	return
+	return float32(tmpr)
 }
 
 // Float64 converting
-func Float64(s interface{}) (r float64) {
-	r, _ = strconv.ParseFloat(String(s), 64)
-	return
+func Float64(s interface{}) float64 {
+	switch v := s.(type) {
+	case int:
+		return float64(v)
+	case int8:
+		return float64(v)
+	case int16:
+		return float64(v)
+	case int32:
+		return float64(v)
+	case int64:
+		return float64(v)
+	case uint:
+		return float64(v)
+	case uint8:
+		return float64(v)
+	case uint16:
+		return float64(v)
+	case uint32:
+		return float64(v)
+	case uint64:
+		return float64(v)
+	case float32:
+		return float64(v)
+	case float64:
+		return float64(v)
+	}
+
+	r, _ := strconv.ParseFloat(String(s), 64)
+	return r
 }
 
 // Map convert to map[string]interface{}
@@ -201,33 +549,11 @@ func IsPtr(s interface{}) bool {
 // IsNumber check number type conformity
 func IsNumber(s interface{}) bool {
 	switch s.(type) {
-	case int:
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64:
 		return true
-	case int8:
-		return true
-	case int16:
-		return true
-	case int32:
-		return true
-	case int64:
-		return true
-	case uint:
-		return true
-	case uint8:
-		return true
-	case uint16:
-		return true
-	case uint32:
-		return true
-	case uint64:
-		return true
-	case float32:
-		return true
-	case float64:
-		return true
-	default:
-		return false
 	}
+
+	return false
 }
 
 // TypeName return name of variable type
